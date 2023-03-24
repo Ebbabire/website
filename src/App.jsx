1866c766bca44defd42f58325a52e336d96ffa58
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-03-24 20:55:17 +0300

    added a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import Navbar from './components/Navbar';
+
 function App() {
-  return <div></div>;
+  return (
+    <div className="flex flex-col justify-between h-screen">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
+import { BsFillPersonFill } from 'react-icons/bs';
+import { GrProjects } from 'react-icons/gr';
+import { MdEmail } from 'react-icons/md';
+
+const Navbar = () => {
+  const [menuOpened, setMenuOpened] = useState(false);
+  return (
+    <>
+      {!menuOpened ? (
+        <nav className="px-4 py-2 shadow-lg">
+          <div className="flex justify-between items-center lg:w-[65%] mx-auto">
+            <div>
+              <a href="#main">
+                <span className="text-3xl font-semibold font-Kalam tracking-wider pr-2">
+                  Ebba
+                </span>
+                <span className="font-extrabold text-3xl text-red-600">.</span>
+              </a>
+            </div>
+            <div className="hidden md:flex">
+              <a href="#main" className="lg:pr-4">
+                <span className="text-lg font-medium tracking-wide hover:bg-slate-200 transition duration-300 ease-in-out rounded-lg py-2 px-4">
+                  Home
+                </span>
+              </a>
+              <a href="#main" className="lg:pr-4">
+                <span className="text-lg font-medium tracking-wide hover:bg-slate-200 transition duration-300 ease-in-out rounded-lg py-2 px-4">
+                  About
+                </span>
+              </a>
+              <a href="#projects" className="lg:pr-4">
+                <span className="text-lg font-medium tracking-wide hover:bg-slate-200 transition duration-300 ease-in-out rounded-lg py-2 px-4">
+                  Projects
+                </span>
+              </a>
+              <a href="#contact" className="lg:pr-4">
+                <span className="text-lg font-medium tracking-wide hover:bg-slate-200 transition duration-300 ease-in-out rounded-lg py-2 px-4">
+                  Contact
+                </span>
+              </a>
+            </div>
+            <div
+              className="md:hidden hover:cursor-pointer"
+              onClick={() => setMenuOpened(!menuOpened)}
+            >
+              <AiOutlineMenu className="text-2xl" />
+            </div>
+          </div>
+        </nav>
+      ) : (
+        <div
+          className="flex flex-col justify-center text-xl items-center h-screen"
+          onClick={() => setMenuOpened(!menuOpened)}
+        >
+          <div className="flex justify-center items-center w-[75%] bg-slate-300 rounded-full py-3 mb-3 transition duration-300 ease-in-out hover:scale-110">
+            <AiFillHome />
+            <a href="#home">
+              <span className="text-xl font-semibold tracking-wide rounded-lg p-2">
+                Home
+              </span>
+            </a>
+          </div>
+          <div className="flex justify-center items-center w-[75%] bg-slate-300 rounded-full py-3 mb-3 transition duration-300 ease-in-out hover:scale-110">
+            <BsFillPersonFill />
+            <a href="#about">
+              <span className="text-xl font-semibold tracking-wide rounded-lg p-2">
+                About
+              </span>
+            </a>
+          </div>
+          <div className="flex justify-center items-center w-[75%] bg-slate-300 rounded-full py-3 mb-3 transition duration-300 ease-in-out hover:scale-110">
+            <GrProjects />
+            <a href="#projects">
+              <span className="text-xl font-semibold tracking-wide rounded-lg p-2">
+                Projects
+              </span>
+            </a>
+          </div>
+          <div className="flex justify-center items-center w-[75%] bg-slate-300 rounded-full py-3 mb-3 transition duration-300 ease-in-out hover:scale-110">
+            <MdEmail />
+            <a href="#contact">
+              <span className="text-xl font-semibold tracking-wide rounded-lg p-2">
+                Contact
+              </span>
+            </a>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
