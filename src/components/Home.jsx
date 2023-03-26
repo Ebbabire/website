8d1c208e78fd059ade12a2734b07bd2932e6de36
Ebba Birhanu <ebba.birhanu@gmail.com>
2023-03-26 14:09:31 +0300

    added a cv section and changed div styling

---
+++ src/components/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import hero from '../assets/hero_image.png';
+import { IoLogoJavascript, IoLogoHtml5, IoLogoCss3 } from 'react-icons/io';
+import { IoLogoReact } from 'react-icons/io5';
+import { SiTailwindcss, SiRedux } from 'react-icons/si';
+
+const Home = () => {
+  return <div className="mt-14"></div>;
+};
+
+export default Home;
